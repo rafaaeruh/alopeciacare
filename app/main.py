9d65a889376b79ffd1ce64dac28455ba53344f4d
rafaaeruh <rafaelangelosantos1@gmail.com>
2026-01-25 16:36:52 -0300

    Initial interface of the history page

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
+#classe Python  que representa a aplicação web
 from flask import Flask
+from routes.history import history_bp
 
+# Instância da classe que represente a aplicação web
 app = Flask(__name__)
 
+app.register_blueprint(history_bp)
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello World!</p> <h1>Bom dia </h1>"
+def home():
+    return 'app rodandoo :D'
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
