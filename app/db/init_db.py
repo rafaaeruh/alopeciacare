ac76da2ffd4e3b7641ec8eb430ed176afc7a798a
rafaaeruh <rafaelangelosantos1@gmail.com>
2026-01-28 08:26:46 -0300

    Adding a function to save session images.

--- app/db/init_db.py
+++ app/db/init_db.py
@@ -15,36 +15,35 @@ conn = sq.connect(DB_PATH)
 
 cursor = conn.cursor()
 
+
+cursor.execute('DROP TABLE IF EXISTS sessoes')
+
+conn.commit()
+
 ## SE TABELA NÃO EXISTIR
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS sessoes (
                id INTEGER PRIMARY KEY AUTOINCREMENT,
-               mes TEXT,
                data TEXT,
                tratamento TEXT,
-               area TEXT,
-               photo_path TEXT,
-               status TEXT,
+               photo_frontal TEXT,
+               photo_entradas TEXT,
+               photo_midscalp TEXT,
+               photo_coroa TEXT,
+               frontal_area TEXT,
+               entradas_area TEXT,
+               midscalp_area TEXT,
+               coroa_area TEXT,
                obs TEXT
                )
 
 ''')
 
+
+
 conn.commit()
 
 
-
-def registrar_sessao(id,mes,data,tratamento,area,imagem_path,status,obs):
-    conn = sq.connect(DB_PATH)
-    cursor = conn.cursor()
-
-    cursor.execute("INSERT INTO sessoes VALUES (?,?,?,?,?,?,?,?)", (id,mes,data,tratamento,area,imagem_path,status,obs))
-
-    conn.commit()
-    conn.close()
-
-
-registrar_sessao(27,"Agosto","19/08/2026","Biotina","Linha frontal","/home/Images","estável","nada a declarar")
 
 cursor.execute('SELECT * FROM sessoes')
 print(cursor.fetchall())
--- app/routes/newsession.py
+++ app/routes/newsession.py
@@ -1,7 +1,28 @@
-from flask import Flask, Blueprint, render_template
+from flask import Flask, Blueprint, render_template, request
+import datetime
+import os 
+
 
 session_bp = Blueprint("session", __name__,url_prefix="/newsession")
 
-@session_bp.route("/")
+@session_bp.route("/", methods=["GET","POST"])
 def session():
+
+    nowdata = datetime.datetime.now().strftime("%Y/%m/%d").replace("/", "-")
+    
+    if request.method == "POST":
+        file1 = request.files["photo_frontal"]
+        file2 = request.files["photo_entradas"]
+        file3 = request.files["photo_midscalp"]
+        file4 = request.files["photo_coroa"]
+        arquivos = [file1,file2,file3,file4]
+        names = ["frontal","entradas","mid_scalp","coroa"]
+
+        for i,file in enumerate(arquivos):
+            if file and file.filename != "":
+                filename = names[i] + "_" + nowdata + ".jpg"
+                upload_path = os.path.join("static", "uploads", filename) 
+                file.save(upload_path)
+
     return  render_template("newsession.html")
+
