e9f89a4a164c9f4c4142fa3d06cab57101930de6
rafaaeruh <rafaelangelosantos1@gmail.com>
2026-01-30 07:44:36 -0300

    Added a function to change slide items on the history page

--- app/routes/history.py
+++ app/routes/history.py
@@ -1,7 +1,35 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 history_bp = Blueprint("history", __name__,url_prefix="/history")
-
-@history_bp.route("/")
+flag = True
+@history_bp.route("/", methods=["GET","POST"])
 def history():
-    return render_template("history.html") 
+
+    global indice, flag
+
+    lista_de_frutas = ["Banana","Maçã","Kiwi","Uvas"]
+
+    if flag:
+        indice = 3
+
+    if request.method == "POST":
+
+        if request.form.get("acao") == "esq":
+            if indice <= 3:
+                pass
+            else:
+                flag = False
+                indice -= 1
+                print(indice)
+        elif request.form.get("acao") == "dir" :
+            if indice >= len(lista_de_frutas) + 1:
+                pass
+            else:
+                indice += 1
+                print(indice)
+
+
+
+
+
+    return render_template("history.html", frutas = lista_de_frutas, index= indice) 
